d7f9840e0d4d4f33381a4aa0c36ceb0b8a95d11d
hammad38 <hammadsami119@gmail.com>
2022-10-07 17:53:24 +0500

    .

--- src/Row.js
+++ src/Row.js
@@ -5,6 +5,7 @@ function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
 
   //every time the row loads this piece of code will runs
+  //if [], run once when the row loads and don't run it again
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
@@ -12,11 +13,20 @@ function Row({ title, fetchURL }) {
       return request;
     }
     fetchData();
-    //if [], run once when the row loads and don't run it again
   }, [fetchURL]);
+
+  // console.log(movies)
+
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row-posters">
+        {/* this is for posters */}
+        {movies.map((movie) => (
+          <img src={movie.poster_path} alt={movie.name} />
+        ))}
+      </div>
     </div>
   );
 }
