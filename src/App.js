1f991c1e4c59cc230f70048b269311e0511c6928
hammad38 <hammadsami119@gmail.com>
2022-10-07 19:59:11 +0500

    adding more categories

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     <div className="App">
         <Row title="NETFLIX ORIGINALS" fetchURL = {requests.fetchNetflixOriginals}/>
         <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+        <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+        <Row title="Documentaries" fetchURL={requests.fetchDocumantaries} />
     </div>
   );
 }
