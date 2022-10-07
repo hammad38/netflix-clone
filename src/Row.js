2762a55a92424b58097945f0bf97182d0e407cb0
hammad38 <hammadsami119@gmail.com>
2022-10-07 19:04:09 +0500

    add css

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
+import "./row.css";
 
-const  base_url = "https://image.tmdb.org/t/p/original/"
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
@@ -17,16 +18,21 @@ function Row({ title, fetchURL }) {
     fetchData();
   }, [fetchURL]);
 
-  console.log(movies)
+  console.log(movies);
 
   return (
     <div className="row">
       <h2>{title}</h2>
 
       <div className="row-posters">
-        {/* this is for posters */}
+        {/*  this is for posters   */}
+
         {movies.map((movie) => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            className="row-poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
