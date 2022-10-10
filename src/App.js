bb037eb0e1f7b6890cf014aa3ad412d4840c1d00
hammad38 <hammadsami119@gmail.com>
2022-10-10 18:11:19 +0500

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import Row from './Row';
-import Banner from './Banner';
-import requests from './request';
-import './App.css';
+import Row from "./Row";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import requests from "./request";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <Banner />
-        <Row title="NETFLIX ORIGINALS" fetchURL = {requests.fetchNetflixOriginals} isLargeRow={true}/>
-        <Row title="Trending Now" fetchURL={requests.fetchTrending} />
-        <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
-        <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
-        <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
-        <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
-        <Row title="Documentaries" fetchURL={requests.fetchDocumantaries} />
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumantaries} />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
