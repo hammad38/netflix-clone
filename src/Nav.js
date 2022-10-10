c3b7db6ca83d11fe59f12880c5e8210ba95c4f28
hammad38 <hammadsami119@gmail.com>
2022-10-10 19:35:16 +0500

    update navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav-black"}`}>
       <img
         className="nav-logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
