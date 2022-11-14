ed89f5b2fc7f95baf8231eaa3b0b355985a579e7
Hammad Shoaib <hammadsami119@gmail.com>
2022-11-14 22:34:32 +0500

    adding responsiveness

--- src/Row.js
+++ src/Row.js
@@ -8,7 +8,7 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("")
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   //every time the row loads this piece of code will runs
   //if [], run once when the row loads and don't run it again
@@ -26,26 +26,26 @@ function Row({ title, fetchURL, isLargeRow }) {
     height: "390",
     width: "100%",
     playerVars: {
-          // https://developers.google.com/youtube/player_parameters
+      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
 
-  // console.log(movies); 
+  // console.log(movies);
 
-  const handleClick = (movie) =>{
-    if(trailerUrl){
-      setTrailerUrl("")
-    }else{
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
       movieTrailer(movie?.name || " ")
-      .then((url)=>{
-        const urlParams = new URLSearchParams(new URL(url).search)
-        // youtube videoId starts with v
-        setTrailerUrl(urlParams.get("v"))
-      })
-      .catch((error)=> console.log(error))
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          // youtube videoId starts with v
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
     }
-  }
+  };
 
   return (
     <div className="row">
@@ -56,26 +56,25 @@ function Row({ title, fetchURL, isLargeRow }) {
 
         {movies.map((movie) => {
           const imageUrl = `${base_url}${
-             isLargeRow ? movie.poster_path : movie.backdrop_path
+            isLargeRow ? movie.poster_path : movie.backdrop_path
           }`;
 
           return (
             <img
               key={movie.id}
-              onClick={()=> handleClick(movie)}
+              onClick={() => handleClick(movie)}
               className={`row-poster ${isLargeRow && "row-posterLarge"}`}
               src={
                 imageUrl === "https://image.tmdb.org/t/p/original/null"
                   ? "https://image.tmdb.org/t/p/original//kXfqcdQKsToO0OUXHcrrNCHDBzO.jpg"
                   : imageUrl
-                
               }
               alt={movie.original_title}
             />
           );
         })}
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}  
       {/* console.log(trailerUrl) */}
     </div>
   );
